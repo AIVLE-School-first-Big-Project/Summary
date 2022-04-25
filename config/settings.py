2f84dea881c6d3240421bf05b17c4aa93419839c
Hungeun Jo <hungeun@naver.com>
2022-04-25 13:27:46 +0900

    STT

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'Board',
+    'meeting.apps.MeetingConfig',
 ]
 
 MIDDLEWARE = [
---
+++ meeting/views.py
@@ -0,0 +1,29 @@
+from contextlib import redirect_stdout
+from urllib import request
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+import requests
+import json
+import speech_recognition as sr
+from .models import Meeting
+
+
+def audio(request):
+    url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
+    rest_api_key = '8a6c86f867f58229747408ae4accab0b'
+    header = {"Content-Type": "application/octet-stream", 
+            "Content-Length": "chunked",
+            "Authorization": "KakaoAK " + rest_api_key,
+            }
+    r = sr.Recognizer()
+    with sr.Microphone(sample_rate=16000) as source:
+        print("기록을 시작하겠습니다!")
+        audio = r.listen(source)
+    res = requests.post(url, headers=header, data=audio.get_raw_data())
+    result_json_string = res.text[res.text.index('{"type":"finalResult"'):res.text.rindex('}')+1]
+    result = json.loads(result_json_string)
+    print(result)
+    # meetings = Meeting.objects
+
+    return HttpResponse(str(result['value']))   
