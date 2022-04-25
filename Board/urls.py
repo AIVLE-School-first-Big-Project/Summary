237cca9d69580577dfbd31d85fd672dfe074268d
minji <hellominji03@naver.com>
2022-04-25 14:02:28 +0900

    댓글 기능 수정 중

--- Board/urls.py
+++ Board/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('write/',views.board_write,name='board_write'),
     path('reading/board/<int:b_no>',views.detail_board,name='detail_board'),
     path('board_list/',views.board_list,name='board_list'),
+    path('board/comment/',views.comment,name='comment'),
 ]
