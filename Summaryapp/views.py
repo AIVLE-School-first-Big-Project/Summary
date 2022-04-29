bc3bfbcc7043487e7e444f2837db076877bbebb2
KimGyeongJun <KimGyeongJun@gmail.com>
2022-04-29 12:52:43 +0900

    upload1 수정

--- Summaryapp/views.py
+++ Summaryapp/views.py
@@ -4,6 +4,11 @@ from .summary import sentence
 from .textrank import textrank
 from Mainapp.models import File
 from django.contrib.auth.models import User
+import pdfminer
+from pdfminer.high_level import extract_text
+from docx import Document
+import pytesseract
+import os
 
 # Create your views here.
 def summary(request):
@@ -46,7 +51,6 @@ def upload1(request):
         
         upload_file = request.FILES.get('file')
         name = upload_file.name
-        size = upload_file.size
         
         if(name.find("txt") > 0):
             with open(name, 'wb') as file:
@@ -59,12 +63,73 @@ def upload1(request):
             text = [line.rstrip('\n') for line in text]
             text = text[0]
             
-            sum_text = sentence(text)
+            os.remove(name)
             
-            keyword = textrank(text)
+            # sum_text = sentence(text)
             
-            return HttpResponse('파일 이름 : %s<br>파일 크기 : %s<br><br>파일 전문 : %s<br><br>파일 요약본 : %s<br><br>핵심어 10개 : %s, %s, %s, %s, %s, %s, %s, %s, %s, %s'
-                                % (name, size, text, sum_text, keyword[0], keyword[1], keyword[2], keyword[3], keyword[4], keyword[5], keyword[6], keyword[7], keyword[8], keyword[9]))
+            # keyword = textrank(text)
+            
+            # keyword = " ".join(result)
+            
+            # return HttpResponse('파일 이름 : %s<br>파일 크기 : %s<br><br>파일 전문 : %s<br><br>파일 요약본 : %s<br><br>핵심어 10개 : %s' % (name, size, text, sum_text, keyword))
+            return HttpResponse('전문<br> %s' % (text))
+            
+        elif(name.find("pdf") > 0):
+            with open(name, 'wb') as file:
+                for chunk in upload_file.chunks():
+                    file.write(chunk)
+                    
+            text = extract_text(name)
+            text = text.replace('\n', ' ')
+            
+            # PDF 전문추출 및 요약
+            # max = 500
+            # result = []
+            # if len(text) > 500:
+            #    for i in range(len(text) // max):
+            #         result.append(sentence(text[i*max:(i+1)*max]))
+            #         result.append('\n')
+            #     result.append(sentence(text[(i+1)*max :]))
+            # else:
+            #     result.append(sentence(text))
+            # result = "\n".join(result)
+            
+            # return HttpResponse('전문 <br>%s<br><br><br><br>요약본<br>%s' %(text, result))
+            
+            os.remove(name)
+            
+            return HttpResponse('전문<br> %s' %(text))
+        
+        elif(name.find("docx") > 0):
+            with open(name, 'wb') as file:
+                for chunk in upload_file.chunks():
+                    file.write(chunk)
+            
+            doc = Document(name)
+            text = []
+            
+            for p in doc.paragraphs:
+                text.append(p.text)
+            
+            text = "\n".join(text)
+            
+            os.remove(name)
+            
+            return HttpResponse('전문<br>%s' %(text))
+        
+        elif(name.find("png") > 0 or name.find("PNG") or name.find("ipg") or name.find("JPG")):
+            with open(name, 'wb') as file:
+                for chunk in upload_file.chunks():
+                    file.write(chunk)
+            
+            text = pytesseract.image_to_string(name, lang = 'kor+eng', config = '-c perserve_interword_spaces=1 --psm 4')
+            text = text.replace('\n', ' ')
+            text.strip()
+            
+            os.remove(name)
+            
+            return HttpResponse('이미지 추출<br>%s' %(text))
+                
         else:
             message = "파일형식이 잘못되었습니다."
             return HttpResponse('%s' % (message) )
