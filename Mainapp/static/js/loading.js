889563e0a2de5cf54cb8f1cb6fa8ead9af8e4ff0
AnJiye <oan215@naver.com>
2022-05-02 15:09:17 +0900

    로딩 화면 추가

---
+++ Mainapp/static/js/loading.js
@@ -0,0 +1,9 @@
+function loading() {
+    $.LoadingOverlay("show", {
+        background       : "rgba(0, 0, 0, 0.5)",
+        image            : "",
+        maxSize          : 60,
+        fontawesome      : "fa fa-spinner fa-pulse fa-fw",
+        fontawesomeColor : "#FFFFFF",
+    });
+}
