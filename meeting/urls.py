809d1e87d503628f732573bfe007ec240809a3cc
KimGyeongJun <KimGyeongJun@gmail.com>
2022-05-09 14:42:16 +0900

    meeting 업로드 구현

--- meeting/urls.py
+++ meeting/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('meetingstart/meeting', views.audio),
     # path('home', views.audio),
     path('meetingstart/', views.meetingstart, name='meetingstart'),
+    path('download/', views.downloadFile, name='downloadFile'),
 
 
 ]
--- meeting/views.py
+++ meeting/views.py
@@ -8,11 +8,19 @@ import requests
 import json
 import speech_recognition as sr
 # from .models import Meeting
+import os
+from django.core.files.storage import FileSystemStorage
+from django.http import FileResponse
+import urllib
+
+global encode_title
 
 def meetingstart(request):
     return render(request,'meetingstart.html')
 
 def audio(request):
+    global encode_title
+
     url = "https://kakaoi-newtone-openapi.kakao.com/v1/recognize"
     rest_api_key = '8a6c86f867f58229747408ae4accab0b'
     header = {"Content-Type": "application/octet-stream", 
@@ -29,4 +37,24 @@ def audio(request):
     print(result)
     voice = result['value']
 
+    title = 'meeting'
+    encode_title = urllib.parse.quote(string=f'{title}.txt')
+
+    file = open(f'media/{encode_title}', 'w', encoding="UTF-8")
+    file.write(voice)                # 파일에 문자열 저장
+    file.close()
+
     return render(request,'voice.html', {'voice' : voice})
+
+
+def downloadFile(request):
+    global encode_title
+
+    file_path = os.path.abspath('media/')
+    file_name = encode_title
+    fs = FileSystemStorage(file_path)
+    response = FileResponse(fs.open(file_name, 'rb'),
+                            content_type='text/plain')
+    response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s'% file_name
+
+    return response
