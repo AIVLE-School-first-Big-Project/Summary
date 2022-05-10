5f5de00c28261e7dec525419b969de6886e231ef
AnJiye <oan215@naver.com>
2022-05-10 10:49:01 +0900

    loading 수정

--- Mainapp/static/js/loading.js
+++ Mainapp/static/js/loading.js
@@ -1,9 +1,23 @@
 function loading() {
     $.LoadingOverlay("show", {
-        background       : "rgba(0, 0, 0, 0.5)",
+        // background       : "rgba(0, 0, 0, 0.5)",
         image            : "",
         maxSize          : 60,
         fontawesome      : "fa fa-spinner fa-pulse fa-fw",
-        fontawesomeColor : "#FFFFFF",
+        // fontawesomeColor : "#FFFFFF",
     });
+}
+
+function text_loading() {
+    $.LoadingOverlay("show", {
+        image       : "",
+        text        : "Loading...",
+        textAnimation : 'fadein',
+    });
+    setTimeout(function(){
+        $.LoadingOverlay("text", '조금만 더 기다려주세요.');
+    }, 5000);
+    setTimeout(function(){
+        $.LoadingOverlay("text", '조금만 더 기다려주세요...');
+    }, 10000);
 }
