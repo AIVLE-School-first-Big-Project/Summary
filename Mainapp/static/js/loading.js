2e57a84a2738c15d3ef26b9e784d496d5a02ca0b
AnJiye <oan215@naver.com>
2022-05-10 10:52:44 +0900

    loading color 수정

--- Mainapp/static/js/loading.js
+++ Mainapp/static/js/loading.js
@@ -1,10 +1,10 @@
 function loading() {
     $.LoadingOverlay("show", {
-        // background       : "rgba(0, 0, 0, 0.5)",
+        background       : "rgba(0, 0, 0, 0.5)",
         image            : "",
         maxSize          : 60,
         fontawesome      : "fa fa-spinner fa-pulse fa-fw",
-        // fontawesomeColor : "#FFFFFF",
+        fontawesomeColor : "#FFFFFF",
     });
 }
 
